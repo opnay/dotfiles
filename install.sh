c035534a18959e57465e6c2d73cd75c9b6c65f88
Kim In Seop <opnayn@gmail.com>
2020-12-24 10:06:07 +0000

    Update

--- install.sh
+++ install.sh
@@ -3,13 +3,24 @@ cat .gitconfig.ext >> ~/.gitconfig
 
 echo "📕 Install powerline fonts"
 echo "- Clone git"
-git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts
+git clone https://github.com/powerline/fonts.git --depth=1
 echo "- Run install script"
-/bin/sh ./install.sh
-
+(cd fonts && /bin/sh install.sh)
 
 echo "📕 Install oh-my-zsh/theme/agnoster.custiom"
 echo "- Copy Theme"
 cp theme/oh-my-zsh/agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/agnoster.zsh-theme
 echo "- Change .zshrc config"
 sed -i 's/ZSH_THEME=.*/ZSH_THEME="agnoster"/gi' ~/.zshrc
+
+echo "📕 Install oh-my-zsh/plugin/autojump"
+echo "- Clone git"
+git clone git://github.com/wting/autojump.git
+echo "- Run install script"
+(cd autojump && python install.py)
+echo "- Add .zshrc"
+sed -i 's/plugins=\((.*)\)/plugins=(\1 autojump)/gi' ~/.zshrc
+
+echo "📕 Install oh-my-zsh/plugin/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=\((.*)\)/plugins=(\1 zsh-syntax-highlighting)/gi' ~/.zshrc
