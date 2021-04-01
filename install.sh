3e3a48b519236f8926d2f94f2ca1aeb58817760f
Kim In Seop <opnayn@gmail.com>
2021-04-01 11:13:13 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,32 @@
-echo "📕 Install .gitconfig"
+echo "🎯 Install .gitconfig"
 git config --global alias.c "commit"
 git config --global alias.cp "cherry-pick"
 git config --global alias.ca "commit --amend"
 git config --global alias.one "log --graph --all --pretty=format:'%h -%C(yellow)%d%C(reset) %s%C(reset)'"
 git config --global alias.f "fetch"
 
-echo "📕 Install powerline fonts"
-echo "- Clone git"
-git clone https://github.com/powerline/fonts.git --depth=1
-echo "- Run install script"
-(cd fonts && /bin/sh install.sh)
+# echo "🎯 Install powerline fonts"
+# echo "📕 Clone git"
+# git clone https://github.com/powerline/fonts.git --depth=1
+# echo "📕 Run install script"
+# (cd fonts && /bin/sh install.sh)
 
-# echo "📕 Install oh-my-zsh/theme/agnoster.custiom"
-# echo "- Copy Theme"
+# echo "🎯 Install oh-my-zsh/theme/agnoster.custiom"
+# echo "📕 Copy Theme"
 # cp theme/oh-my-zsh/agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/agnoster.zsh-theme
-# echo "- Change .zshrc config"
+# echo "📕 Change .zshrc config"
 # sed -i 's/ZSH_THEME=.*/ZSH_THEME="agnoster"/gi' ~/.zshrc
 
-echo "📕 Install oh-my-zsh/plugin/autojump"
-echo "- Clone git"
+echo "🎯 Install oh-my-zsh/plugin/autojump"
+echo "📕 Clone git"
 git clone git://github.com/wting/autojump.git
-echo "- Run install script"
+echo "📕 Run install script"
 (cd autojump && python install.py)
-echo "- Add .zshrc"
+echo "📕 Add .zshrc"
 sed -i 's/plugins=(\(.*\))/plugins=(\1 autojump)/gi' ~/.zshrc
 
-echo "📕 Install oh-my-zsh/plugin/zsh-syntax-highlighting"
+echo "🎯 Install oh-my-zsh/plugin/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 sed -i 's/plugins=(\(.*\))/plugins=(\1 zsh-syntax-highlighting)/gi' ~/.zshrc
+
+echo "✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅"
